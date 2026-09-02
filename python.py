9a5b64b99dc50d08d15fa1abf1a23f287aec33bf
murtazafalasiya18-52 <murtazafalasiya18@gmail.com>
2026-09-02 09:01:23 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
