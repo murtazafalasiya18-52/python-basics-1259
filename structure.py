a4590a08ad00fe9288e641443c29c458d4d117cc
murtazafalasiya18-52 <murtazafalasiya18@gmail.com>
2026-09-02 09:20:22 +0530

    Create structure.py

---
+++ structure.py
@@ -0,0 +1,11 @@
+class student:
+  def _init_(self, name, age, marks):
+    self.name = name
+    self.age = age
+    self.marks = marks
+
+    student1 = student("Murtaza , 20 , 85)
+
+    print("Name:", student1.name)
+    print("Age:" , student1.age)
+    print("Marks:" , student1.marks)
